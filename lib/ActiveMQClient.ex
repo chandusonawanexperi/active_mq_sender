aa8acc8c423da00d91b176800d5a42a524e196dd
Chandu Sonawane <chandu.sonawane@xperi.com>
2024-12-11 10:28:56 +0530

    Fixing error while sending the activemq msg

--- lib/ActiveMQClient.ex
+++ lib/ActiveMQClient.ex
@@ -19,21 +19,28 @@ defmodule ActiveMQClient do
       passcode: @password
     ]
 
-    # Establish a connection
-    case StompClient.connect(options) do
-      {:ok, conn} ->
-        # Send the message to the specified queue
-        case StompClient.send(conn, @queue, message) do
+     # Connect to ActiveMQ
+     case StompClient.connect(options) do
+      pid when is_pid(pid) ->
+        IO.puts("Connected successfully: #{inspect(pid)}")
+
+        # Send the message
+        case StompClient.send(pid, @queue, message) do
           :ok ->
             IO.puts("Message sent successfully!")
-            StompClient.disconnect(conn)
+            StompClient.disconnect(pid)
 
           {:error, reason} ->
             IO.puts("Failed to send message: #{inspect(reason)}")
+            StompClient.disconnect(pid)
+
+          unexpected ->
+            IO.puts("Unexpected response from send: #{inspect(unexpected)}")
+            StompClient.disconnect(pid)
         end
 
-      {:error, reason} ->
-        IO.puts("Failed to connect to ActiveMQ: #{inspect(reason)}")
+      unexpected ->
+        IO.puts("Failed to connect to ActiveMQ. Response: #{inspect(unexpected)}")
     end
   end
 end
